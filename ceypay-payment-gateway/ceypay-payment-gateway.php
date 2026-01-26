b597ae5cbcb59b57c3390c4d4e64582eb886f585
Buddhi Thilakshana <hellobachchi@gmail.com>
2026-01-26 21:52:57 +0530

    fix: Add missing version parameter to Facebook Events script
    - Replace null version with CEYPAY_VERSION for wp_enqueue_script call
    - Ensures proper cache busting for external Facebook Events script
    - Resolves WordPress coding standards warning
    
    Version: 1.2.7

--- ceypay-payment-gateway/ceypay-payment-gateway.php
+++ ceypay-payment-gateway/ceypay-payment-gateway.php
@@ -3,7 +3,7 @@
  * Plugin Name: CeyPay Payment Gateway
  * Plugin URI:  https://docs.ceypay.io/
  * Description: WooCommerce payment gateway for CeyPay IPG.
- * Version:     1.2.6
+ * Version:     1.2.7
  * Author:      CeyPay
  * Author URI:  https://ceypay.io/
  * Text Domain: ceypay-payment-gateway
